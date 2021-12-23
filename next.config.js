486202f14fe33fbb2a2e32001c813fb17d6966a1
bqx619 <bqx619@gmail.com>
2021-12-23 12:30:57 +0800

    feat: using swc optimeze

--- next.config.js
+++ next.config.js
@@ -1,25 +1,40 @@
 /** @type {import('next').NextConfig} */
+//
+// const withAntdLess = require('next-plugin-antd-less')
+// const withPlugins = require('next-compose-plugins')
+//
+// const pluginAntdLess = withAntdLess({
+//   // modifyVars: {
+//   //   '@THEME--DARK': 'theme-dark',
+//   // },
+//   lessVarsFilePath: './src/styles/variables.less',
+//   lessVarsFilePathAppendToEndOfContent: false,
+//   // cssLoaderOptions: {
+//   // esModule: false,
+//   // sourceMap: false,
+//   // modules: {
+//   // mode: 'local',
+//   // localIdentName: '[hash:2]',
+//   // },
+//   // },
+// });
+//
+// module.exports = withPlugins([[pluginAntdLess]], {
+//   reactStrictMode: false,
+//   env: {
+//     PEOPLELAND_INFURA_KEY: 'dbffd247db694ccdb6506ddc175c48c6',
+//     PEOPLELAND_ALCHEMY_KEY: '64gGTjEbQ13TNWug6z3SJWLKQTOHRVJ9',
+//     PEOPLELAND_ETHERSCAN_KEY: '7J1KDENKGC567QX8GUHGS6WN1JMVBK8CSK',
+//     SEO_TITLE: 'Peopleland',
+//     SEO_DESCRIPTION: 'For the PEOPLE of ConstitutionDAO who made history.',
+//     SEO_TWITTER: '@peopleland_',
+//   },
+//   webpack(config) {
+//     return config;
+//   },
+// })
 
-const withAntdLess = require('next-plugin-antd-less')
-const withPlugins = require('next-compose-plugins')
-
-const pluginAntdLess = withAntdLess({
-  // modifyVars: {
-  //   '@THEME--DARK': 'theme-dark',
-  // },
-  lessVarsFilePath: './src/styles/variables.less',
-  lessVarsFilePathAppendToEndOfContent: false,
-  // cssLoaderOptions: {
-  // esModule: false,
-  // sourceMap: false,
-  // modules: {
-  // mode: 'local',
-  // localIdentName: '[hash:2]',
-  // },
-  // },
-});
-
-module.exports = withPlugins([[pluginAntdLess]], {
+module.exports = {
   reactStrictMode: false,
   env: {
     PEOPLELAND_INFURA_KEY: 'dbffd247db694ccdb6506ddc175c48c6',
@@ -29,7 +44,4 @@ module.exports = withPlugins([[pluginAntdLess]], {
     SEO_DESCRIPTION: 'For the PEOPLE of ConstitutionDAO who made history.',
     SEO_TWITTER: '@peopleland_',
   },
-  webpack(config) {
-    return config;
-  },
-})
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
+import "../styles/globals.css"
 import type { AppProps } from 'next/app'
 import { Web3ReactProvider } from '@web3-react/core'
 import {ethers} from "ethers";
 import { ApolloProvider } from '@apollo/client';
 import  ApolloClient from '../lib/apolloClient'
 import { ConfigProvider } from 'antd';
-
-require("../styles/globals.less")
 
 function getLibrary(provider: any) {
   return new ethers.providers.Web3Provider(provider)// this will vary according to whether you use e.g. ethers or web3.js
